6a0a28d15d65af0dd5b76c122f9ba84f901e3e2f
marcuspereiragithub <vinnycoast@gmail.com>
2017-11-03 18:25:05 -0200

    New command to add score

--- advinhação/advinhação.c
+++ advinhação/advinhação.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    printf("*******************************************\n");
+    printf("*****************************************\n");
     printf("* Bem vindo ao nosso jogo de advinhação *\n");
-    printf("*******************************************\n");
+    printf("*****************************************\n");
     
-    int numeroSecreto = 42;
+    int segundos = time(0);
+    srand(segundos);
+    
+    int numeroGrande = rand();
+    
+    int numeroSecreto = numeroGrande % 100;
     int chute;       
     int tentativas = 1;
+    double pontos = 1000;
     
     while("true"){
         
@@ -37,9 +45,13 @@ int main() {
                 printf("Seu chute foi menor que o número secreto!\n\n");
             }
         tentativas++;
+        
+        double pontosPerdidos = (abs)(chute - numeroSecreto) / (double)2;
+        pontos = pontos - pontosPerdidos;
       
     }
    
     printf("FIM DO JOGO!\n\n");
-    printf("Você acertou em %d tentativas\n\n", tentativas);
+    printf("Você acertou em %d tentativas\n", tentativas);
+    printf("Total de pontos: %.1f\n", pontos);
 }
